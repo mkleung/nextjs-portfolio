066d114a5a8233e928dc492291a62cb3d0eb585b
Michael Leung <mpakleung@gmail.com>
2025-02-21 18:32:26 -0500

    hero section

--- README.md
+++ README.md
@@ -3,13 +3,19 @@ https://www.youtube.com/watch?v=ELssXP1sTC8
 
 ## Getting Started
 
+```npm install```
+
 First, run the development server:
 
-npm run dev
+```npm run dev```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Open [http://localhost:3000](http://localhost:3000)
 
 
+Notes:
 
-Cool browser
+- Cool browser
 https://polypane.app/
+
+- Prettier Shortcut
+Shift + Alt + F
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,46 @@
+import memojiImage from "@/assets/images/memoji-computer.png";
+import Image from "next/image";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className="py-32">
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <Image
+            src={memojiImage}
+            className="size-[100px]"
+            alt="Person peeking from behind laptop"
+          />
+          <div className="bg-blue-800 border border-gray-800 mt-5 px-4 py-1.5 inline-flex items-center rounded-lg">
+            <div className="text-sm font-medium">
+              Available for new projects
+            </div>
+          </div>
+          <div className="max-w-lg mx-auto">
+            <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+              Building Exceptional User Experiences
+            </h1>
+
+            <p className="mt-4 text-center text-white/60 md:text-lg">
+              I specialize in transforming designs into functional,
+              high-performing web applications. Let's discuss your next project.
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center md:flex-row justify-center">
+            <button className="inline-flex items-center gap-2 border-white/10 px-6 h-12 rounded-xl">
+              <span className="font-semibold">Explore My Work</span>
+              <ArrowDown className="size-4" />
+            </button>
+
+            <button className="bg-white rounded-xl px-5 py-1">
+              <span>👋</span>
+              <span className="text-black">Let's Connect</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
