1f9b51742758c91680b51cfddfb3e3ce7037cece
Michael Leung <mpakleung@gmail.com>
2025-02-24 14:29:40 -0500

    tape section

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,31 @@
+import StarIcon from "@/assets/icons/star.svg";
+
+const words = [
+  "Accessible",
+  "Secure",
+  "Multilingual suppport",
+  "Mobile Friendly",
+  "Scalable",
+  "Fast Loading",
+  "SEO Optimized",
+  "Analytics Driven",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className="py-16 overflow-x-clip">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 " >
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-4 py-3 ">
+            {words.map((word) => (
+              <div key={word} className="inline-flex gap-4 items-center">
+                <span className="text-gray-900 uppercase font-extrabold text-sm">{word}</span>
+                <StarIcon className="size-6 text-gray-900" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
