3f98a46c340e1c54a8de9f12d6b620c50ec9372b
Michael <mpakleung@gmail.com>
2025-03-02 02:06:32 -0500

    content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from 'next';
-import { Inter, Calistoga } from 'next/font/google';
+import { Inter, Outfit } from 'next/font/google';
 import './globals.css';
 import { twMerge } from 'tailwind-merge'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
-const calistoga = Calistoga({
+// const calistoga = Calistoga({
+//   subsets: ['latin'],
+//   variable: '--font-serif',
+//   weight: '400',
+// });
+
+const outfit = Outfit({
   subsets: ['latin'],
   variable: '--font-serif',
   weight: '400',
@@ -24,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={twMerge(inter.variable, calistoga.variable, "bg-gray-800 text-white antialiased font-sans")}>{children}</body>
+      <body className={twMerge(inter.variable, outfit.variable, "bg-gray-800 text-white antialiased font-sans")}>{children}</body>
     </html>
   );
 }
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,6 +1,7 @@
 import memojiImage from "@/assets/images/memoji-computer.png";
 import Image from "next/image";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
+import logo from "@/assets/images/mike.jpeg";
 
 export const HeroSection = () => {
   return (
@@ -8,8 +9,8 @@ export const HeroSection = () => {
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
-            src={memojiImage}
-            className="size-[100px]"
+            src={logo}
+            className="size-[100px] rounded-full"
             alt="Person peeking from behind laptop"
           />
           <div className="bg-blue-800 border border-gray-800 mt-5 px-4 py-1.5 inline-flex items-center rounded-lg">
@@ -17,9 +18,9 @@ export const HeroSection = () => {
               Available for new projects
             </div>
           </div>
-          <div className="max-w-lg mx-auto">
-            <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
-              Building Exceptional User Experiences
+          <div className="max-w-2xl mx-auto">
+            <h1 className="font-serif text-3xl md:text-5xl text-center mt-8">
+            Design 👉 Develop 👉 Deliver
             </h1>
 
             <p className="mt-4 text-center text-white/60 md:text-lg">
@@ -28,7 +29,7 @@ export const HeroSection = () => {
             </p>
           </div>
 
-          <div className="flex flex-col items-center md:flex-row justify-center">
+          <div className="flex flex-col items-center md:flex-row justify-center mt-5">
             <button className="inline-flex items-center gap-2 border-white/10 px-6 h-12 rounded-xl">
               <span className="font-semibold">Explore My Work</span>
               <ArrowDown className="size-4" />
