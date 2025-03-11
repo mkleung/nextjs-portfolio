520329fcfdbdf49ccd456de25998c6b8b71e6229
Michael <mpakleung@gmail.com>
2025-03-11 02:11:33 -0400

    fix drag issue

--- src/components/about-section/AboutCard copy.tsx
+++ src/components/about-section/AboutCard copy.tsx
@@ -55,7 +55,7 @@ export const AboutCard = () => {
     >
       <CardHeader title="About Me" className="" />
 
-      <div className="flex flex-col p-6 md:py-8 md:px-10 md:pt-0 gap-4" ref={constraintRef}>
+      <div className="flex flex-col p-6 md:py-8 md:px-10 md:pt-0 gap-4">
         <p className="text-white/85 ">
           Hello! I&apos;m a dedicated front-end developer with a passion for
           delivering user-friendly websites that captivates audiences. I also
@@ -68,15 +68,15 @@ export const AboutCard = () => {
           <h3 className="font-serif text-xl gradient-underline">Hobbies</h3>
         </div>
 
-        <div className=" h-[80px] mt-1" >
+        <div className="h-full mt-1" ref={constraintRef}>
           {hobbies.map((hobby) => (
             <motion.div
               key={hobby.title}
-              className="hover:pointer relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-white rounded-full group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 me-2 mb-2"
-              whileTap={{ scale: 0.95 }}
-              whileHover={{ scale: 1.05 }}
+              className="hover:pointer relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-white rounded-full group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 me-2 mb-2 hover:cursor-grab active:cursor-grabbing"
               drag
               dragConstraints={constraintRef}
+              whileTap={{ scale: 0.95 }}
+              whileHover={{ scale: 1.05 }}
             >
               <span className="relative inline-flex items-center px-5 py-1 transition-colors ease-in duration-200 bg-gray-700 dark:bg-gray-700 rounded-full group-hover:bg-transparent group-hover:dark:bg-transparent group-hover:text-black">
                 {hobby.title}
